b3640b958d1f536eba7e0bd55c285d57b1ab9a62
menghanxia <menghanxyz@gmail.com>
2022-10-07 21:25:52 +0800

    upload whole project

--- app.py
+++ app.py
@@ -1,7 +1,92 @@
 import gradio as gr
+import os, requests
+from inference import setup_model, colorize_grayscale, predict_anchors
 
-def greet(name):
-    return "Hello " + name + "!!"
+## download checkpoint
+def download_file_from_google_drive(id, destination):
+    def get_confirm_token(response):
+        for key, value in response.cookies.items():
+            if key.startswith('download_warning'):
+                return value
+        return None
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch()
+    def save_response_content(response, destination):
+        CHUNK_SIZE = 32768
+        with open(destination, "wb") as f:
+            for chunk in response.iter_content(CHUNK_SIZE):
+                if chunk: # filter out keep-alive new chunks
+                    f.write(chunk)
+
+    URL = "https://docs.google.com/uc?export=download"
+    session = requests.Session()
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+    save_response_content(response, destination)
+
+id = "1J4vB6kG4xBLUUKpXr5IhnSSa4maXgRvQ"
+destination = "disco-beta.pth.rar"
+download_file_from_google_drive(id, destination)
+os.rename("disco-beta.pth.tar", "./checkpoints/disco-beta.pth.tar")
+
+## step 1: set up model
+device = "cuda"
+checkpt_path = "./checkpoints/disco-beta.pth.tar"
+assert os.path.exists(checkpt_path), "No checkpoint found!"
+colorizer, colorLabeler = setup_model(checkpt_path, device=device)
+
+def click_colorize(rgb_img, hint_img, n_anchors, is_high_res, is_editable):
+    if hint_img is None:
+        hint_img = rgb_img
+    output = colorize_grayscale(colorizer, colorLabeler, rgb_img, hint_img, n_anchors, is_high_res, is_editable, device)
+    return output
+
+def click_predanchors(rgb_img, n_anchors, is_high_res, is_editable):
+    output = predict_anchors(colorizer, colorLabeler, rgb_img, n_anchors, is_high_res, is_editable, device)
+    return output
+
+## step 2: configure interface
+def switch_states(is_checked):
+    if is_checked:
+        return gr.Image.update(visible=True), gr.Button.update(visible=True)
+    else:
+        return gr.Image.update(visible=False), gr.Button.update(visible=False)
+
+demo = gr.Blocks(title="DISCO: Image Colorization")
+with demo:
+    gr.Markdown(value="""**DISCO: image colorization that disentangles color multimodality and spatial affinity via global anchors**.""")
+    with gr.Row():
+        with gr.Column(scale=1):
+            Image_input = gr.Image(type="numpy", label="Input", interactive=True)
+            Image_anchor = gr.Image(type="numpy", label="Anchor", tool="color-sketch", interactive=True, visible=False)
+            with gr.Row():
+                Num_anchor = gr.Number(type="int", value=8, label="Num. of anchors (3~14)")
+                Radio_resolution = gr.Radio(type="index", choices=["Low (256x256)", "High (512x512)"], \
+                                            label="Colorization resolution", value="Low (256x256)")
+            Ckeckbox_editable = gr.Checkbox(default=False, label='Show editable anchors')
+            with gr.Row():
+                Button_show_anchor = gr.Button(value="Predict anchors", visible=False)
+                Button_run = gr.Button(value="Colorize")
+        with gr.Column(scale=1):
+            Image_output = gr.Image(type="numpy", label="Output", shape=[100,100])
+    
+    Ckeckbox_editable.change(fn=switch_states, inputs=Ckeckbox_editable, outputs=[Image_anchor, Button_show_anchor])
+    Button_show_anchor.click(fn=click_predanchors, inputs=[Image_input, Num_anchor, Radio_resolution, Ckeckbox_editable], outputs=Image_anchor)
+    Button_run.click(fn=click_colorize, inputs=[Image_input, Image_anchor, Num_anchor, Radio_resolution, Ckeckbox_editable], \
+                    outputs=Image_output)
+    ## guiline
+    gr.Markdown(value="""
+                    **Guideline**
+                    1. Upload your image;
+                    2. Set up the arguments: "Num. of anchors" and "Colorization resolution";
+                    3. Two modes are supported:
+                        - **Editable**: check ""Show editable anchors" and click "Predict anchors". Then, modify the colors of the predicted anchors (anchor mask will be applied afterward). Finally, click "Colorize" to get the result.
+                        - **Automatic**: click "Colorize" to get the automatically colorized output.
+                    
+                    *To know more about the method, please refer to our project page: [https://menghanxia.github.io/projects/disco.html](https://menghanxia.github.io/projects/disco.html)*
+                    """)
+    
+demo.launch(server_name='9.134.253.83',server_port=7788)
---
+++ inference.py
@@ -0,0 +1,109 @@
+import os, glob, sys, logging
+import argparse, datetime, time
+import numpy as np
+import cv2
+from PIL import Image
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from models import model, basic
+from utils import util
+
+
+def setup_model(checkpt_path, device="cuda"):
+    seed = 130
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    #print('--------------', torch.cuda.is_available())
+    """Load the model into memory to make running multiple predictions efficient"""
+    colorLabeler = basic.ColorLabel(device=device)
+    colorizer = model.AnchorColorProb(inChannel=1, outChannel=313, enhanced=True, colorLabeler=colorLabeler)
+    colorizer = colorizer.to(device)
+    #checkpt_path = "./checkpoints/disco-beta.pth.rar"
+    assert os.path.exists(checkpt_path)
+    data_dict = torch.load(checkpt_path, map_location=torch.device('cpu'))
+    colorizer.load_state_dict(data_dict['state_dict'])
+    colorizer.eval()
+    return colorizer, colorLabeler
+
+
+def resize_ab2l(gray_img, lab_imgs, vis=False):
+    H, W = gray_img.shape[:2]
+    reszied_ab = cv2.resize(lab_imgs[:,:,1:], (W,H), interpolation=cv2.INTER_LINEAR)
+    if vis:
+        gray_img = cv2.resize(lab_imgs[:,:,:1], (W,H), interpolation=cv2.INTER_LINEAR)
+        return np.concatenate((gray_img[:,:,np.newaxis], reszied_ab), axis=2)
+    else:
+        return np.concatenate((gray_img, reszied_ab), axis=2)
+
+def prepare_data(rgb_img, target_res):
+    rgb_img = np.array(rgb_img / 255., np.float32)
+    lab_img = cv2.cvtColor(rgb_img, cv2.COLOR_RGB2LAB)
+    org_grays = (lab_img[:,:,[0]]-50.) / 50.
+    lab_img = cv2.resize(lab_img, target_res, interpolation=cv2.INTER_LINEAR)
+        
+    lab_img = torch.from_numpy(lab_img.transpose((2, 0, 1)))
+    gray_img = (lab_img[0:1,:,:]-50.) / 50.
+    ab_chans = lab_img[1:3,:,:] / 110.
+    input_grays = gray_img.unsqueeze(0)
+    input_colors = ab_chans.unsqueeze(0)
+    return input_grays, input_colors, org_grays
+
+
+def colorize_grayscale(colorizer, color_class, rgb_img, hint_img, n_anchors, is_high_res, is_editable, device="cuda"):
+    n_anchors = int(n_anchors)
+    n_anchors = max(n_anchors, 3)
+    n_anchors = min(n_anchors, 14)
+    target_res = (512,512) if is_high_res else (256,256)
+    input_grays, input_colors, org_grays = prepare_data(rgb_img, target_res)
+    input_grays = input_grays.to(device)
+    input_colors = input_colors.to(device)
+    
+    if is_editable:
+        print('>>>:editable mode')
+        sampled_T = -1
+        _, input_colors, _ = prepare_data(hint_img, target_res)
+        input_colors = input_colors.to(device)
+        pal_logit, ref_logit, enhanced_ab, affinity_map, spix_colors, hint_mask = colorizer(input_grays, \
+                                                                input_colors, n_anchors, sampled_T)
+    else:
+        print('>>>:automatic mode')
+        sampled_T = 0
+        pal_logit, ref_logit, enhanced_ab, affinity_map, spix_colors, hint_mask = colorizer(input_grays, \
+                                                                input_colors, n_anchors, sampled_T)
+
+    pred_labs = torch.cat((input_grays,enhanced_ab), dim=1)
+    lab_imgs = basic.tensor2array(pred_labs).squeeze(axis=0)
+    lab_imgs = resize_ab2l(org_grays, lab_imgs)
+        
+    lab_imgs[:,:,0] = lab_imgs[:,:,0] * 50.0 + 50.0
+    lab_imgs[:,:,1:3] = lab_imgs[:,:,1:3] * 110.0
+    rgb_output = cv2.cvtColor(lab_imgs[:,:,:], cv2.COLOR_LAB2RGB)
+    return (rgb_output*255.0).astype(np.uint8)
+
+
+def predict_anchors(colorizer, color_class, rgb_img, n_anchors, is_high_res, is_editable, device="cuda"):
+    n_anchors = int(n_anchors)
+    n_anchors = max(n_anchors, 3)
+    n_anchors = min(n_anchors, 14)
+    target_res = (512,512) if is_high_res else (256,256)
+    input_grays, input_colors, org_grays = prepare_data(rgb_img, target_res)
+    input_grays = input_grays.cuda(non_blocking=True)
+    input_colors = input_colors.cuda(non_blocking=True)
+                
+    sampled_T, sp_size = 0, 16
+    pal_logit, ref_logit, enhanced_ab, affinity_map, spix_colors, hint_mask = colorizer(input_grays, \
+                                                            input_colors, n_anchors, sampled_T)
+    pred_probs = pal_logit
+    guided_colors = color_class.decode_ind2ab(ref_logit, T=0)
+    guided_colors = basic.upfeat(guided_colors, affinity_map, sp_size, sp_size)
+    anchor_masks = basic.upfeat(hint_mask, affinity_map, sp_size, sp_size)
+    marked_labs = basic.mark_color_hints(input_grays, guided_colors, anchor_masks, base_ABs=None)
+    lab_imgs = basic.tensor2array(marked_labs).squeeze(axis=0)
+    lab_imgs = resize_ab2l(org_grays, lab_imgs, vis=True)
+        
+    lab_imgs[:,:,0] = lab_imgs[:,:,0] * 50.0 + 50.0
+    lab_imgs[:,:,1:3] = lab_imgs[:,:,1:3] * 110.0
+    rgb_output = cv2.cvtColor(lab_imgs[:,:,:], cv2.COLOR_LAB2RGB)
+    return (rgb_output*255.0).astype(np.uint8)
