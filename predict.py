9f8e276041ee91584a2f2f59a7a0720d969ad294
camenduru <54370274+camenduru@users.noreply.github.com>
2024-02-07 00:39:25 +0000

    tets

--- predict.py
+++ predict.py
@@ -5,20 +5,20 @@ import os, requests
 import numpy as np
 from inference import setup_model, colorize_grayscale, predict_anchors
     
-def inference(rgb_img, hint_img, n_anchors, is_high_res, is_editable, colorizer, colorLabeler):
-    if hint_img is None:
-        hint_img = rgb_img
-    output = colorize_grayscale(colorizer, colorLabeler, rgb_img, hint_img, n_anchors, is_high_res, is_editable, device)
+def inference(rgb_img, n_anchors, is_high_res, is_editable, colorizer, colorLabeler):
+    hint_img = rgb_img
+    output = colorize_grayscale(colorizer, colorLabeler, rgb_img, hint_img, n_anchors, is_high_res, is_editable, "cpu")
     return output
 
 class Predictor(BasePredictor):
     def setup(self) -> None:
+        os.system("wget https://huggingface.co/menghanxia/disco/resolve/main/disco-beta.pth.rar -O checkpoints/disco-beta.pth.rar")
         device = "cpu"
         checkpt_path = "checkpoints/disco-beta.pth.rar"
-        self.colorizer, self.colorLabeler = setup_model(checkpt_path, device=device)
+        self.colorizer, self.colorLabeler = setup_model(checkpt_path, device="cpu")
     def predict(
         self,
-        input_image: Path = Input(description="Input Image"),
+        rgb_img: Path = Input(description="Input Image"),
     ) -> Path:
-        output_image = inference(rgb_img, hint_img, n_anchors, is_high_res, is_editable, self.colorizer, self.colorLabeler)
+        output_image = inference(rgb_img, 8, "High (512x512)", False, self.colorizer, self.colorLabeler)
         return Path(output_image)
